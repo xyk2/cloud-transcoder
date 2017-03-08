f1a7541cdbc553287f5c8cc460bbb84effc0dd1a
Xiaoyang Kao <contact.xykao@gmail.com>
2017-03-09 00:20:24 +0800

    Force fluent-ffmpeg version 2.0.1 to resolve events not firing issue
    - Ensure wss.broadcast bodies are all JSON
    - Detect if running on Google Cloud with process.env.PWD (hack)
    - Different directories for production and development
    - Return total files uploaded and how many total

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var fs = require('fs');
+var fs = require('fs-extra');
 var restify = require('restify');
 var ffmpeg = require('fluent-ffmpeg');
 const WebSocket = require('ws');
@@ -6,26 +6,30 @@ var dir = require('node-dir');
 var path = require('path');
 var request = require('request');
 
+console.log(process.env);
 
-var options = {
-  url: 'http://metadata.google.internal/computeMetadata/v1/instance/id',
-  headers: {
-    'Metadata-Flavor': 'Google'
-  }
-};
+if(process.env.PWD == '/usr/local/ffmpeg-runner/broadcast.cx-ffmpeg-runner') { // If there is a response from metadata server, means it is running on GCS
+	_OUTPUT_PATH = '/usr/local/ffmpeg-runner/broadcast.cx-ffmpeg-runner/_outputs';
+	_PRESETS_PATH = '/usr/local/ffmpeg-runner/broadcast.cx-ffmpeg-runner/presets';
+	_PORT = 80;
+	_WS_PORT = 80;
 
-request(options, function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-});
+} else { // Running local
+	_OUTPUT_PATH = '/Users/XYK/Desktop/ffmpeg_outputs';
+	_PRESETS_PATH = '/Users/XYK/Desktop/Dropbox/broadcast.cx-ffmpeg-runner/presets';
+	ffmpeg.setFfmpegPath('/Users/XYK/Desktop/ffmpeg'); // Explicitly set ffmpeg and ffprobe paths
+	ffmpeg.setFfprobePath('/Users/XYK/Desktop/ffprobe');
+	_PORT = 8080;
+	_WS_PORT = 8081;
+}
+
 
 
 var server = restify.createServer({
 	name: 'ffmpeg-runner'
 });
 
-const wss = new WebSocket.Server({ port: 8081 });
+const wss = new WebSocket.Server({ port: _WS_PORT });
 
 wss.broadcast = function broadcast(data) {
 	wss.clients.forEach(function each(client) {
@@ -44,136 +48,169 @@ var gcs = google_cloud.storage();
 var bucket = gcs.bucket('broadcast-cx-raw-recordings');
 var dest_bucket = gcs.bucket('broadcast-cx-sandbox');
 
-//ffmpeg.setFfmpegPath('/Users/XYK/Desktop/ffmpeg'); // Explicitly set ffmpeg and ffprobe paths
-//ffmpeg.setFfprobePath('/Users/XYK/Desktop/ffprobe');
 
-server.get('/transcode/HLS/:filename', hlsTranscode);
-server.get('/upload/', upload);
+server.get('/transcode/hls/:filename', hlsTranscode);
 
 
 function hlsTranscode(req, res, next) {
-	if(!req.params.filename) return res.send(400, {status: 1, message: "Invalid Request."});
+	if(!req.params.filename) {
+		wss.broadcast(JSON.stringify({'event': 'error', 'message': 'Invalid request.'}));
+		res.send(400, {status: 1, message: "Invalid Request."});
+	}
 
 	_transcodedRenditionsCount = 0;
 
 	HD_720P_TRANSCODE = function(filename, callback) { 
-		ffmpeg(filename, { presets: '/usr/local/ffmpeg-runner/broadcast.cx-ffmpeg-runner/presets' }).preset('hls')
+		_HD_720P = ffmpeg(filename, { presets: _PRESETS_PATH }).preset('hls')
 		.videoBitrate(3000)
 		.audioBitrate('96k')
 		.size('1280x720')
-		.saveToFile('/usr/local/ffmpeg-runner/broadcast.cx-ffmpeg-runner/_outputs/720p_3000k.m3u8')
+		.saveToFile(_OUTPUT_PATH + '/720p_3000k.m3u8')
 		.on('start', function(commandLine) {
-			console.log('FFMPEG: ' + commandLine);
-			wss.broadcast('FFMPEG: ' + commandLine);
+			_ret = {'event': 'start', 'rendition': '720P_3000K', 'command': commandLine};
+
+		    console.log(JSON.stringify(_ret));
+		    wss.broadcast(JSON.stringify(_ret));
+
 		})
-		.on('progress', function(progress) {
-			console.log('Processing: ' + JSON.stringify(progress) + ' frames');
+		.on('progress', function(progress) {			
+			progress['event'] = 'progress';
+			progress['rendition'] = '720P_3000K';
+
+			console.log(JSON.stringify(progress));
 			wss.broadcast(JSON.stringify(progress));
 		})
 		.on('stderr', function(stderrLine) {
-		    //console.log('Stderr output: ' + stderrLine);
+		    console.log('Stderr output: ' + stderrLine);
 		})
 		.on('error', function(err, stdout, stderr) {
-		    console.log('Cannot process video: ' + err.message);
-		    wss.broadcast("Cannot process video: " + err.message);
+			_ret = {'event': 'error', 'message': err.message};
+			wss.broadcast(JSON.stringify(_ret));
 		})
 		.on('end', function(stdout, stderr) {
-		    console.log('Transcoding succeeded!');
-		    wss.broadcast('Transcoding successful.');
+			_ret = {'event': 'success', 'rendition': '720P_3000K'};
+
+		    console.log(JSON.stringify(_ret));
+		    wss.broadcast(JSON.stringify(_ret));
+
 		    _transcodedRenditionsCount++;
 		    callback();
 		});
 	}
 
 	SD_480P_TRANSCODE = function(filename, callback) {
-		ffmpeg(filename, { presets: '/usr/local/ffmpeg-runner/broadcast.cx-ffmpeg-runner/presets' }).preset('hls')
+		_SD_480P = ffmpeg(filename, { presets: _PRESETS_PATH }).preset('hls')
 		.videoBitrate(1500)
 		.audioBitrate('96k')
 		.size('854x480')
-		.saveToFile('/usr/local/ffmpeg-runner/broadcast.cx-ffmpeg-runner/_outputs/480p_1500k.m3u8')
+		.saveToFile(_OUTPUT_PATH + '/480p_1500k.m3u8')
 		.on('start', function(commandLine) {
-			console.log('FFMPEG: ' + commandLine);
-			wss.broadcast('FFMPEG: ' + commandLine);
+			_ret = {'event': 'start', 'rendition': '480P_1500K', 'command': commandLine};
+
+		    console.log(JSON.stringify(_ret));
+		    wss.broadcast(JSON.stringify(_ret));
 		})
-		.on('progress', function(progress) {
-			console.log('Processing: ' + JSON.stringify(progress) + ' frames');
+		.on('progress', function(progress) {			
+			progress['event'] = 'progress';
+			progress['rendition'] = '480P_1500K';
+
+			console.log(JSON.stringify(progress));
 			wss.broadcast(JSON.stringify(progress));
 		})
 		.on('stderr', function(stderrLine) {
 		    //console.log('Stderr output: ' + stderrLine);
 		})
 		.on('error', function(err, stdout, stderr) {
-		    console.log('Cannot process video: ' + err.message);
-		    wss.broadcast("Cannot process video: " + err.message);
+			_ret = {'event': 'error', 'message': err.message};
+			wss.broadcast(JSON.stringify(_ret));
 		})
 		.on('end', function(stdout, stderr) {
-		    console.log('Transcoding succeeded!');
-		    wss.broadcast('Transcoding successful.');
+			_ret = {'event': 'success', 'rendition': '480P_1500K'};
+
+		    console.log(JSON.stringify(_ret));
+		    wss.broadcast(JSON.stringify(_ret));
+
 		    _transcodedRenditionsCount++;
 		    callback();
 		});
 	}
 
 	SD_360P_TRANSCODE = function(filename, callback) {
-		ffmpeg(filename, { presets: '/usr/local/ffmpeg-runner/broadcast.cx-ffmpeg-runner/presets' }).preset('hls')
+		_SD_360P = ffmpeg(filename, { presets: _PRESETS_PATH }).preset('hls')
 		.videoBitrate(850)
 		.audioBitrate('96k')
 		.size('640x360')
-		.saveToFile('/usr/local/ffmpeg-runner/broadcast.cx-ffmpeg-runner/_outputs/360p_850k.m3u8')
+		.saveToFile(_OUTPUT_PATH + '/360p_850k.m3u8')
 		.on('start', function(commandLine) {
-			console.log('FFMPEG: ' + commandLine);
-			wss.broadcast('FFMPEG: ' + commandLine);
+			_ret = {'event': 'start', 'rendition': '360P_850K', 'command': commandLine};
+
+		    console.log(JSON.stringify(_ret));
+		    wss.broadcast(JSON.stringify(_ret));
 		})
-		.on('progress', function(progress) {
-			console.log('Processing: ' + JSON.stringify(progress) + ' frames');
+		.on('progress', function(progress) {			
+			progress['event'] = 'progress';
+			progress['rendition'] = '360P_850K';
+
+			console.log(JSON.stringify(progress));
 			wss.broadcast(JSON.stringify(progress));
 		})
 		.on('stderr', function(stderrLine) {
 		    //console.log('Stderr output: ' + stderrLine);
 		})
 		.on('error', function(err, stdout, stderr) {
-		    console.log('Cannot process video: ' + err.message);
-		    wss.broadcast("Cannot process video: " + err.message);
+			_ret = {'event': 'error', 'message': err.message};
+			wss.broadcast(JSON.stringify(_ret));
 		})
 		.on('end', function(stdout, stderr) {
-		    console.log('Transcoding succeeded!');
-		    wss.broadcast('Transcoding successful.');
+			_ret = {'event': 'success', 'rendition': '360P_850K'};
+
+		    console.log(JSON.stringify(_ret));
+		    wss.broadcast(JSON.stringify(_ret));
+
 		    _transcodedRenditionsCount++;
 		    callback();
 		});
 	}
 
 	SD_240P_TRANSCODE = function(filename, callback) {
-		ffmpeg(filename, { presets: '/usr/local/ffmpeg-runner/broadcast.cx-ffmpeg-runner/presets' }).preset('hls')
+		_SD_240P = ffmpeg(filename, { presets: _PRESETS_PATH }).preset('hls')
 		.videoBitrate(400)
 		.audioBitrate('96k')
 		.size('352x240')
-		.saveToFile('/usr/local/ffmpeg-runner/broadcast.cx-ffmpeg-runner/_outputs/240p_400k.m3u8')
+		.saveToFile(_OUTPUT_PATH + '/240p_400k.m3u8')
 		.on('start', function(commandLine) {
-			console.log('FFMPEG: ' + commandLine);
-			wss.broadcast('FFMPEG: ' + commandLine);
+			_ret = {'event': 'start', 'rendition': '240P_400K', 'command': commandLine};
+
+		    console.log(JSON.stringify(_ret));
+		    wss.broadcast(JSON.stringify(_ret));
 		})
-		.on('progress', function(progress) {
-			console.log('Processing: ' + JSON.stringify(progress) + ' frames');
+		.on('progress', function(progress) {			
+			progress['event'] = 'progress';
+			progress['rendition'] = '240P_400K';
+
+			console.log(JSON.stringify(progress));
 			wss.broadcast(JSON.stringify(progress));
 		})
 		.on('stderr', function(stderrLine) {
 		    //console.log('Stderr output: ' + stderrLine);
 		})
 		.on('error', function(err, stdout, stderr) {
-		    console.log('Cannot process video: ' + err.message);
-		    wss.broadcast("Cannot process video: " + err.message);
+			_ret = {'event': 'error', 'message': err.message};
+			wss.broadcast(JSON.stringify(_ret));
 		})
 		.on('end', function(stdout, stderr) {
-		    console.log('Transcoding succeeded!');
-		    wss.broadcast('Transcoding successful.');
+			_ret = {'event': 'success', 'rendition': '240P_400K'};
+
+		    console.log(JSON.stringify(_ret));
+		    wss.broadcast(JSON.stringify(_ret));
+
 		    _transcodedRenditionsCount++;
 		    callback();
 		});
 	}
 
 	CREATE_INDEX_M3U8 = function() {
-		fs.createReadStream('index.m3u8').pipe(fs.createWriteStream('/usr/local/ffmpeg-runner/broadcast.cx-ffmpeg-runner/_outputs/index.m3u8'));
+		fs.createReadStream('index.m3u8').pipe(fs.createWriteStream(_OUTPUT_PATH + '/index.m3u8'));
 	}
 
 	bucket.file(req.params.filename).download({
@@ -181,26 +218,36 @@ function hlsTranscode(req, res, next) {
 	}, function(err) {
 		if(err) {
 			console.log(err.code, err.message);
+			wss.broadcast(JSON.stringify({'event': 'error', 'message': err.code + ' ' + err.message}));
 			res.send({status: 0, message: err.code + ' ' + err.message});
 			return;
 		} // Error handling (bucket file not found in GCS)
 
-		res.send({status: 0, message: "Transcodes started"});
 
-		CREATE_INDEX_M3U8();
-		HD_720P_TRANSCODE(req.params.filename, uploadToGCS);
-		SD_480P_TRANSCODE(req.params.filename, uploadToGCS);
-		SD_360P_TRANSCODE(req.params.filename, uploadToGCS);
-		SD_240P_TRANSCODE(req.params.filename, uploadToGCS);
+		fs.emptyDir(_OUTPUT_PATH, err => {
+			if (err) return console.error(err);
+
+			res.send({status: 0, message: "Transcodes started", file: req.params.filename});
+		  	CREATE_INDEX_M3U8();
+		  	HD_720P_TRANSCODE(req.params.filename, uploadToGCS);
+		  	SD_480P_TRANSCODE(req.params.filename, uploadToGCS);
+		  	SD_360P_TRANSCODE(req.params.filename, uploadToGCS);
+		  	SD_240P_TRANSCODE(req.params.filename, uploadToGCS);
+		});
+
+
 	});
 
 	// Will not execute until all 4
-
 	function uploadToGCS() {
 		if(_transcodedRenditionsCount != 4) return;
 
-		dir.files('/usr/local/ffmpeg-runner/broadcast.cx-ffmpeg-runner/_outputs', function(err, files) {
+		dir.files(_OUTPUT_PATH, function(err, files) {
 			if (err) throw err;
+
+			_total_files_count = files.length;
+			_uploaded_files_count = 0; // Use this rather than index because indexes are called asynchronously
+
 			files.forEach(function(file, index) {
 				if(path.extname(file) === '.m3u8' || path.extname(file) === '.ts') { // Only upload M3U8s and transport streams
 
@@ -225,62 +272,26 @@ function hlsTranscode(req, res, next) {
 						}
 
 						gFileObj.setMetadata(metadata, function(err, apiResponse) {});
-						console.log('Uploaded ' + gFileObj.name);
-						wss.broadcast('Uploaded ' + gFileObj.name);
+						_uploaded_files_count++;
+
+
+						_ret = {'event': 'gcsupload', 'file': gFileObj.name, 'uploadedCount': _uploaded_files_count, 'totalCount': _total_files_count};
+						wss.broadcast(JSON.stringify(_ret));
+						console.log(JSON.stringify(_ret));
 					});
 				}
 			});
 		});
-
-
-
-
 	}
-
-
-}
-
-function upload(req, res, next) {
-	dir.files('/usr/local/ffmpeg-runner/broadcast.cx-ffmpeg-runner/_outputs', function(err, files) {
-	    if (err) throw err;
-	    files.forEach(function(file, index) {
-	    	if(path.extname(file) === '.m3u8' || path.extname(file) === '.ts') { // Only upload M3U8s and transport streams
-	    		dest_bucket.upload(file, function(err, file) {
-	    			if(err) { // Error handling
-	    				console.log(err);
-	    				return;
-	    			}
-
-	    			if(file.name.indexOf('.m3u8') != -1) {
-	    				var metadata = {
-	    				  contentType: 'application/x-mpegURL'
-	    				};
-	    			} else {
-	    				var metadata = {
-	    				  contentType: 'video/MP2T'
-	    				};
-	    			}
-
-	    			file.setMetadata(metadata, function(err, apiResponse) {});
-
-	    			console.log("Successful upload");
-	    			
-	    		});
-	    	}
-	    });
-	});
 }
 
 
 
-
-if(process.env.NODE_ENV === 'production') _port = 80;
-else _port = 8080;
-
-
-server.listen(_port, function() {
+server.listen(_PORT, function() {
 	console.log('%s listening at %s', server.name, server.url);
 });
+
+
 
 
 
